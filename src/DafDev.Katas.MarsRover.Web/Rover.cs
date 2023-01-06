0df6a843aa1aefe52e423008cfdf94a997929ded
Daf <afahd.ali@gmail.com>
2023-01-06 16:31:06 +0100

    Implemented forward movement
    * red test
    
    * test is now green
    
    * New red test with east position
    
    * turn test green again
    
    * refactored code
    
    * refactored tests
    
    * added mirror tests for west and south directions
    
    * turned tests green
    
    * small refactor

--- src/DafDev.Katas.MarsRover.Web/Rover.cs
+++ src/DafDev.Katas.MarsRover.Web/Rover.cs
@@ -1,17 +1,40 @@
-using System.Drawing;
+using DafDev.Katas.MarsRover.Web.Navigation;
 
 namespace DafDev.Katas.MarsRover.Web;
 
 public class Rover
 {
-    public Point StartingPoint { get; set; }
+    public Coordinates Position { get; set; }
     public char Direction { get; set; }
 
-    public void GetCommands(char[] commands){ }
+    public void GetCommands(char[] commands)
+    {
+        foreach (var command in commands)
+        {
+            switch (command)
+            {
+                case 'f': MoveForward(Direction); break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void MoveForward(char direction)
+    {
+        switch (direction)
+        {
+            case 'N': Position.Y++; break;
+            case 'E': Position.X++; break;
+            case 'S': Position.Y--; break;
+            case 'W': Position.X--; break;
+            default: break;
+        }
+    }
 
     public Rover Init() => new()
     {
-        StartingPoint= new Point(0, 0),
+        Position= new Coordinates(0, 0),
         Direction = 'N',
     };
 }
--- tests/DafDev.Katas.MarsRover.Web.Tests/RoverTests.cs
+++ tests/DafDev.Katas.MarsRover.Web.Tests/RoverTests.cs
@@ -8,14 +8,15 @@ public class RoverTests
 
     public RoverTests()
     {
-        _target= new Rover();
+        _target = new Rover();
     }
 
     [Fact]
-    public void ReturnsStartingPointAndDirection()
+    public void InitFuntion_ReturnsDefaultStartingPointAndDirection()
     {
         var result = _target.Init();
-        Assert.Equal(new Point(0,0), result.StartingPoint);
+        Assert.Equal(0, result.Position.X);
+        Assert.Equal(0, result.Position.Y);
         Assert.Equal('N', result.Direction);
     }
 
@@ -24,5 +25,29 @@ public class RoverTests
     {
         var commands = Array.Empty<char>();
         _target.GetCommands(commands);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetCommandsData))]
+    public void ForwardCommandMovesRoverForwardBy1UnitFromStartingPosition(char direction, int expectedX, int expectedY)
+    {
+        var rover = _target.Init();
+        rover.Direction = direction;
+        var commands = new[] { 'f' };
+
+        rover.GetCommands(commands);
+
+        Assert.Equal(direction, rover.Direction);
+        Assert.Equal(expectedX, rover.Position.X);
+        Assert.Equal(expectedY, rover.Position.Y);
+
+    }
+
+    public static IEnumerable<object[]> GetCommandsData()
+    {
+        yield return new object[] { 'N', 0, 1 };
+        yield return new object[] { 'E', 1, 0 };
+        yield return new object[] { 'S', 0, -1 };
+        yield return new object[] { 'W', -1, 0 };
     }
 }
