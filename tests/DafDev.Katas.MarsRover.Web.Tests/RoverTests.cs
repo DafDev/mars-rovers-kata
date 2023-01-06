57c740409d4d7cb0dfd9fbd5bed569bca6aa4d20
Daf <afahd.ali@gmail.com>
2023-01-06 16:16:36 +0100

    added mirror tests for west and south directions

--- tests/DafDev.Katas.MarsRover.Web.Tests/RoverTests.cs
+++ tests/DafDev.Katas.MarsRover.Web.Tests/RoverTests.cs
@@ -47,5 +47,7 @@ public class RoverTests
     {
         yield return new object[] { 'N', 0, 1 };
         yield return new object[] { 'E', 1, 0 };
+        yield return new object[] { 'S', 0, -1 };
+        yield return new object[] { 'W', -1, 0 };
     }
 }
