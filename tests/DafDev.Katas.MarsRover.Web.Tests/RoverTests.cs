6761facfbd5c6cfc944a8891da2f5630dcce3ed4
Daf <afahd.ali@gmail.com>
2023-01-06 11:55:22 +0100

    wrote first test that turns red

--- tests/DafDev.Katas.MarsRover.Web.Tests/RoverTests.cs
+++ tests/DafDev.Katas.MarsRover.Web.Tests/RoverTests.cs
@@ -2,9 +2,19 @@ namespace DafDev.Katas.MarsRover.Web;
 
 public class RoverTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly Rover _target;
 
+    public RoverTests()
+    {
+        _target= new Rover();
+    }
+
+    [Fact]
+    public void ReturnsStartingPointAndDirection()
+    {
+        var result = _target.Init();
+        Assert.Equal(0, result.X);
+        Assert.Equal(0, result.Y);
+        Assert.Equal('N', result.Direction);
     }
 }
