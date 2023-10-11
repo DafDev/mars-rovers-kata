66274ae03f61decae6dc8ca5a522f5b4c2f697ff
Daf <afahd.ali@gmail.com>
2023-10-11 18:08:37 +0200

    added domain tests

---
+++ tests/DafDev.Katas.MarsRover.Navigation.Domain.Tests/Data/DriverTestData.cs
@@ -0,0 +1,42 @@
+using DafDev.Katas.MarsRover.Navigation.Domain.Models;
+
+namespace DafDev.Katas.MarsRover.Navigation.Application.Tests.Data;
+public class DriverTestData
+{
+    public static IEnumerable<object[]> GetForwardCommandData()
+    {
+        yield return new object[] { CardinalDirections.North, new Coordinates(0, 0), new Coordinates(0, 1) };
+        yield return new object[] { CardinalDirections.North, new Coordinates(0, int.MinValue), new Coordinates(0, int.MaxValue) };
+        yield return new object[] { CardinalDirections.East, new Coordinates(0, 0), new Coordinates(1, 0) };
+        yield return new object[] { CardinalDirections.South, new Coordinates(0, 0), new Coordinates(0, -1) };
+        yield return new object[] { CardinalDirections.South, new Coordinates(0, int.MaxValue), new Coordinates(0, int.MinValue) };
+        yield return new object[] { CardinalDirections.West, new Coordinates(0, 0), new Coordinates(-1, 0) };
+        
+    }
+
+    public static IEnumerable<object[]> GetBackwardCommandData()
+    {
+        yield return new object[] { CardinalDirections.North, new Coordinates(0, 0), new Coordinates(0, -1) };
+        yield return new object[] { CardinalDirections.North, new Coordinates(0, int.MaxValue), new Coordinates(0, int.MinValue) };
+        yield return new object[] { CardinalDirections.East, new Coordinates(0, 0), new Coordinates(-1, 0) };
+        yield return new object[] { CardinalDirections.South, new Coordinates(0, 0), new Coordinates(0, 1) };
+        yield return new object[] { CardinalDirections.South, new Coordinates(0, int.MinValue), new Coordinates(0, int.MaxValue) };
+        yield return new object[] { CardinalDirections.West, new Coordinates(0, 0), new Coordinates(1, 0) };
+    }
+
+    public static IEnumerable<object[]> GetTurnRightCommandData()
+    {
+        yield return new object[] { CardinalDirections.North, CardinalDirections.East };
+        yield return new object[] { CardinalDirections.East, CardinalDirections.South };
+        yield return new object[] { CardinalDirections.South, CardinalDirections.West };
+        yield return new object[] { CardinalDirections.West, CardinalDirections.North };
+    }
+
+    public static IEnumerable<object[]> GetTurnLeftCommandData()
+    {
+        yield return new object[] { CardinalDirections.North, CardinalDirections.West };
+        yield return new object[] { CardinalDirections.West, CardinalDirections.South };
+        yield return new object[] { CardinalDirections.South, CardinalDirections.East };
+        yield return new object[] { CardinalDirections.East, CardinalDirections.North };
+    }
+}
---
+++ tests/DafDev.Katas.MarsRover.Navigation.Domain.Tests/Services/DriverTests.cs
@@ -0,0 +1,96 @@
+using DafDev.Katas.MarsRover.Navigation.Domain.Exceptions;
+using DafDev.Katas.MarsRover.Navigation.Domain.Models;
+using DafDev.Katas.MarsRover.Navigation.Domain.Services;
+using DafDev.Katas.MarsRover.Navigation.Domain.Tests.Data;
+
+namespace DafDev.Katas.MarsRover.Navigation.Domain.Tests.Services;
+public class DriverTests
+{
+    private readonly DriverServices _sut = new();
+
+    [Theory]
+    [MemberData(nameof(DriverTestData.GetForwardCommandData), MemberType = typeof(DriverTestData))]
+    public void MoveForwardMovesRoverForwardBy1UnitFromStartingPosition(CardinalDirections direction,
+        Coordinates startingCoordinates, Coordinates expectedCoordinates)
+    {
+        //Act
+        var result = _sut.MoveForward(startingCoordinates, direction);
+
+        //Assert
+        result.Should().Be(expectedCoordinates);
+    }
+
+    [Theory]
+    [MemberData(nameof(DriverTestData.GetBackwardCommandData), MemberType = typeof(DriverTestData))]
+    public void MoveBackwardMovesRoverBackwardBy1UnitFromStartingPosition(CardinalDirections direction,
+        Coordinates startingCoordinates, Coordinates expectedCoordinates)
+    {
+        //Act
+        var result = _sut.MoveBackward(startingCoordinates, direction);
+
+        //Assert
+        result.Should().Be(expectedCoordinates);
+    }
+
+    [Theory]
+    [MemberData(nameof(DriverTestData.GetTurnRightCommandData), MemberType = typeof(DriverTestData))]
+    public void TurnRightMovesTheDirectionClockwise(CardinalDirections startingDirection, CardinalDirections expectedDirection)
+    {
+        //Act
+        var result = _sut.TurnRight(startingDirection);
+
+        //Assert
+        result.Should().Be(expectedDirection);
+    }
+
+    [Theory]
+    [MemberData(nameof(DriverTestData.GetTurnLeftCommandData), MemberType = typeof(DriverTestData))]
+    public void TurnLefttMovesTheDirectionCounterClockwise(CardinalDirections startingDirection, CardinalDirections expectedDirection)
+    {
+        //Act
+        var result = _sut.TurnLeft(startingDirection);
+
+        //Assert
+        result.Should().Be(expectedDirection);
+    }
+
+    [Fact]
+    public void MoveBackwardWithUnknownDirectionThrowsUnknownCardinalDirectionException()
+    {
+        //Arrange & Act
+        var action = () => _sut.MoveBackward(new(0, 0), (CardinalDirections)5);
+
+        //Assert
+        action.Should().Throw<UnknownCardinalDirectionException>();
+    }
+
+    [Fact]
+    public void MoveForwardWithUnknownDirectionThrowsUnknownCardinalDirectionException()
+    {
+        //Arrange & Act
+        var action = () => _sut.MoveForward(new(0, 0), (CardinalDirections)5);
+
+        //Assert
+        action.Should().Throw<UnknownCardinalDirectionException>();
+    }
+
+    [Fact]
+    public void TurnLeftdWithUnknownDirectionThrowsUnknownCardinalDirectionException()
+    {
+        //Arrange & Act
+        var action = () => _sut.TurnLeft((CardinalDirections)5);
+
+        //Assert
+        action.Should().Throw<UnknownCardinalDirectionException>();
+    }
+
+    [Fact]
+    public void TurnRightdWithUnknownDirectionThrowsUnknownCardinalDirectionException()
+    {
+        //Arrange & Act
+        var action = () => _sut.TurnRight((CardinalDirections)5);
+
+        //Assert
+        action.Should().Throw<UnknownCardinalDirectionException>();
+    }
+}
