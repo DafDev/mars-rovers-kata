6eca7d4ac4395145e984381275ed4f5b1d4a3998
Daf <afahd.ali@gmail.com>
2023-10-11 18:18:57 +0200

    test refactoring

--- tests/DafDev.Katas.MarsRover.Navigation.Domain.Tests/Data/DriverTestData.cs
+++ tests/DafDev.Katas.MarsRover.Navigation.Domain.Tests/Data/DriverTestData.cs
@@ -1,6 +1,6 @@
 using DafDev.Katas.MarsRover.Navigation.Domain.Models;
 
-namespace DafDev.Katas.MarsRover.Navigation.Application.Tests.Data;
+namespace DafDev.Katas.MarsRover.Navigation.Domain.Tests.Data;
 public class DriverTestData
 {
     public static IEnumerable<object[]> GetForwardCommandData()
